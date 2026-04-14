87dddaf40c45a33c1dd36e9a08a51c299f027aaa
Ashukr321 <coderashukr321@gmail.com>
2026-04-14 12:32:57 +0530

    feat(sparse):understand the sparse array

--- 01-basics/05-sparse-arrays/practice.js
+++ 01-basics/05-sparse-arrays/practice.js
@@ -11,3 +11,14 @@ console.log(sparseArr);
 // 3. Set the length to 5 and log the array.
 // 4. Delete the first element and log the array.
 
+
+//array where some of index have empty value , index don't have actual value  assigned 
+// in sparse array some index are missing 
+
+// ways  to create sparse array 
+const num = [1, , 2];
+console.log(num);
+//  using constructor 
+const arr = new Array(5);
+console.log(arr);
+//  assign at higher index value it will created sparse array 
