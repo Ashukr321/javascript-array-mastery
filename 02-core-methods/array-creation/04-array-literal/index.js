c5a48238b3dd379399ccd5ce02a5736582b27930
Ashukr321 <coderashukr321@gmail.com>
2026-04-15 09:05:23 +0530

    feat(array):creation of array using array literal

---
+++ 02-core-methods/array-creation/04-array-literal/index.js
@@ -0,0 +1,5 @@
+// array literal are the simplest ways to create the array in the array in 
+// we use [] : square bracket to create the array 
+let arr = [1,2,3];
+console.log(arr);
+// these are the array literal 
