a75a2c437bb481ee9128cb5c1f2f4b816653ade4
Ashukr321 <coderashukr321@gmail.com>
2026-04-20 20:20:14 +0530

    feat(array):splice methods in js

--- 02-core-methods/mutating-methods/08-splice/README.md
+++ 02-core-methods/mutating-methods/08-splice/README.md
@@ -1,4 +1,17 @@
-# splice
+# splice 
+
+<!-- add  : element
+remove  ::remove element from array
+replace :: replace prev value with new value  -->
+
+element from array 
 
 This folder is for practicing splice.
+
+<!-- splice methods 
+
+splice(start,deleteNUmberOFcOUNT, element add );
+ -->
+
+ 
 
--- 02-core-methods/mutating-methods/08-splice/practice.js
+++ 02-core-methods/mutating-methods/08-splice/practice.js
@@ -2,3 +2,19 @@
 
 // Add your practice code here
 
+let number =[1,2,4];
+number.splice(0,1,5); 
+// add element in the start of the array 
+
+console.log(number);
+
+
+// remove item from array 
+
+const data = [1,2,4,4,4,4];
+data.splice(1,3); // 1 index se 3  element will be removed 
+console.log(data);
+
+data.splice(0,0,1,2,6); // mutate the original array 
+console.log(data);
+// splice methods is also use of the replacing the array element 
