2f1cedc114a19c613ee859f8532ec82ab48fdd69
Ashukr321 <coderashukr321@gmail.com>
2026-04-21 13:22:37 +0530

    feat(array):non-mutating array filter*create the new array and it will return the array of the element that passes the condiiton

--- 02-core-methods/non-mutating-methods/02-filter/practice.js
+++ 02-core-methods/non-mutating-methods/02-filter/practice.js
@@ -1,4 +1,29 @@
 // Practice: filter
+// it create the new array it is a hof : in callback function 
+// each item of the array goes and check condition if the condition is true then added into newly created  array 
+// and return it 
 
-// Add your practice code here
+// it does't mutate the original array 
 
+
+
+// filter((currentElement,index, array)=>{});
+
+// index is refer to the current index  of the element 
+// array on which we apply the filter methods 
+// we can also apply on array of object 
+
+const users = [
+    {name :"ASHUTOSH",age:21},
+    {
+        name:"amit",age:17,
+    },
+    {
+        name:"sumit",age:25
+    }
+]
+
+const adult  = users.filter((user)=>user.age>=18);
+
+console.log(adult);
+// widely use in the react application for the filtering the data;
