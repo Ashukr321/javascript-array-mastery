6a1a84a56ba1dc04b7abc498d2e22d2774c2a7c2
Ashukr321 <coderashukr321@gmail.com>
2026-04-21 17:49:05 +0530

    feat(array):toReversed array methods in js

--- 02-core-methods/non-mutating-methods/10-toReversed/README.md
+++ 02-core-methods/non-mutating-methods/10-toReversed/README.md
@@ -1,4 +1,6 @@
 # toReversed
 
-This folder is for practicing toReversed.
+toReverse methods is the the js array methods that non mutating methods :
+this methods don't mutate the original array : this will return reverse method and original array is safe 
+
 
--- 02-core-methods/non-mutating-methods/10-toReversed/practice.js
+++ 02-core-methods/non-mutating-methods/10-toReversed/practice.js
@@ -1,4 +1,8 @@
 // Practice: toReversed
 
-// Add your practice code here
+const arrNum = [1,2,3,4,5,5,2,2,3,4];
 
+const reversedArr  = arrNum.toReversed(); // here this methods reverse the original array item without mutating the original array 
+
+console.log(reversedArr);
+console.log(arrNum);
