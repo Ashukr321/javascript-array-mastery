3fbe3d2cab3e224bb22abce3d2b605681162fcd3
Ashukr321 <coderashukr321@gmail.com>
2026-04-21 17:04:29 +0530

    feat(reduce):array reduce methods

--- 02-core-methods/non-mutating-methods/07-reduce/README.md
+++ 02-core-methods/non-mutating-methods/07-reduce/README.md
@@ -1,4 +1,10 @@
 # reduce
 
-This folder is for practicing reduce.
+reduce methods is use for store the all array into single value : sum 
+struct: 
 
+<!-- arr.reduce((accumulator,currentElement,index,array)=>{
+    // logic
+},accumulator_Initial_Value) -->
+
+// result will store into the accumulator variable 
--- 02-core-methods/non-mutating-methods/07-reduce/practice.js
+++ 02-core-methods/non-mutating-methods/07-reduce/practice.js
@@ -1,4 +1,22 @@
 // Practice: reduce
 
-// Add your practice code here
+const numArr = [1,2,3,4,5];
 
+const resultSum = numArr.reduce((acc,curr)=>{
+    return acc = acc+curr;
+},0);
+
+// default this return undefined so we need to  must return the accumulator;
+
+console.log(resultSum);
+
+
+const arr = [[1, 2], [3, 4], [5,[6]]];
+
+const flat = arr.reduce((acc,curr)=>{
+   return acc.concat(Array.isArray(curr) ? flatMap(curr) : curr);
+},[])// our accumulator intial will be : [] empty array in which we store 
+
+console.log(flat);   
+// flat array 
+// nested or recursive flat array 
