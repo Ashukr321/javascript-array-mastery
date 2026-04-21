016ea8d95fdb515b9e4136bb2064320f1714c587
Ashukr321 <coderashukr321@gmail.com>
2026-04-21 17:53:58 +0530

    feat(array):splice methods

--- 02-core-methods/non-mutating-methods/12-toSpliced/README.md
+++ 02-core-methods/non-mutating-methods/12-toSpliced/README.md
@@ -1,4 +1,7 @@
 # toSpliced
 
-This folder is for practicing toSpliced.
+this methods is similar to splice methods : where splice method is the mutating methods that will mutate the original array 
+// but toSplice methods does't mutate the original array this will return the new array after performing the operator 
+
+
 
--- 02-core-methods/non-mutating-methods/12-toSpliced/practice.js
+++ 02-core-methods/non-mutating-methods/12-toSpliced/practice.js
@@ -1,4 +1,9 @@
 // Practice: toSpliced
 
-// Add your practice code here
+const fruits = ["apple", "banana", "cherry"]; // array literals
+
+const result = fruits.toSpliced(1, 1, "mango"); // target index is 1 , add remove  1 , and add mango
+
+console.log(result); // ["apple", "mango", "cherry"] ; as expected output 
+console.log(fruits); // ["apple", "banana", "cherry"] ✅ unchanged
 
