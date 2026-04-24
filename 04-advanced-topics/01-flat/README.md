38c94286ef5c78111ba49318189d50b12a06ab39
Ashukr321 <coderashukr321@gmail.com>
2026-04-24 08:41:41 +0530

    feat(array):flat methods

--- 04-advanced-topics/01-flat/README.md
+++ 04-advanced-topics/01-flat/README.md
@@ -1,4 +1,9 @@
 # flat
 
-This folder is for practicing flat.
+flat(): by default this flat the array at 1 level 
 
+
+
+
+
+// flat(depthparameter ): how depthe we want to flat it : we pass it 
--- 04-advanced-topics/01-flat/practice.js
+++ 04-advanced-topics/01-flat/practice.js
@@ -1,4 +1,31 @@
 // Practice: flat
+// const arr = [12,3,[3,4]];
 
-// Add your practice code here
+// console.log(arr.flat());// [12,3,3,4];
 
+// const arr = [1, [2, [3, [4]]]];
+
+// console.log(arr.flat(1)); // [1, 2, [3, [4]]]
+// console.log(arr.flat(2)); // [1, 2, 3, [4]]
+// console.log(arr.flat(3)); // [1, 2, 3, 4]
+
+
+// // infinity 
+
+// console.log(arr.flat(Infinity))// flat all level of depth 
+
+// // default depth is 1: 
+
+
+// Polyfill idea (interview level 🔥)
+//  use recursive 
+function flatArray(arr) {
+  return arr.reduce((acc, curr) => {
+    return acc.concat(Array.isArray(curr) ? flatArray(curr) : curr);
+  }, []);
+}
+
+
+
+const arr = [1, [2, [3, [4]]]];
+console.log(flatArray(arr));
