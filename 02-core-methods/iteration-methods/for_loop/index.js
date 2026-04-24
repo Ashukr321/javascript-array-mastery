8e8391c2558034dfce93f937379d07ab8811e07a
Ashukr321 <coderashukr321@gmail.com>
2026-04-24 08:14:17 +0530

    feat(array):for loop

--- 02-core-methods/iteration-methods/for_loop/index.js
+++ 02-core-methods/iteration-methods/for_loop/index.js
@@ -1,1 +1,17 @@
 // for  loop
+
+
+const arr = [1,3,4,5,5,6];
+for(let i = 0; i<arr.length; i+=2){
+    console.log(arr[i])// 
+}
+
+// variable declare i = 0 ; 
+// i<arr.length
+// statement execute 
+// increment i++
+//  checked condition 
+// statement execute 
+// repeat 
+
+//  this is the simple looping use for iterate the array items 
