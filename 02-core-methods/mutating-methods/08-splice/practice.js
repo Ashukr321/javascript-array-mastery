d52ecff94d6ac7198be2114f1ccb6dc9ee19a6df
Ashukr321 <coderashukr321@gmail.com>
2026-05-05 21:13:27 +0530

    Feat(array):splice methods in js

--- 02-core-methods/mutating-methods/08-splice/practice.js
+++ 02-core-methods/mutating-methods/08-splice/practice.js
@@ -2,8 +2,8 @@
 
 // Add your practice code here
 
-let number =[1,2,4];
-number.splice(0,1,5); 
+let number = [1, 2, 4];
+number.splice(0, 1, 5);
 // add element in the start of the array 
 
 console.log(number);
@@ -11,10 +11,20 @@ console.log(number);
 
 // remove item from array 
 
-const data = [1,2,4,4,4,4];
-data.splice(1,3); // 1 index se 3  element will be removed 
+const data = [1, 2, 4, 4, 4, 4];
+data.splice(1, 3); // 1 index se 3  element will be removed 
 console.log(data);
 
-data.splice(0,0,1,2,6); // mutate the original array 
+data.splice(0, 0, 1, 2, 6); // mutate the original array
 console.log(data);
-// splice methods is also use of the replacing the array element 
+// // splice methods is also use of the replacing the array element 
+let arr = [10, 20, 30, 40];
+
+// Replace 20 with 99
+arr.splice(1, 1, 99);
+
+console.log(arr); // [10, 99, 30, 40]
+
+// here we see splice(startIndex, number of element deleteCount , add item );
+
+
